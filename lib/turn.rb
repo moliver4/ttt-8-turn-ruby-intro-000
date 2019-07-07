46bf05461f0058be6e6e46086ecc73c40ba4fb2e
Tiffany Tang <tifftang22@gmail.com>
2019-07-07 21:23:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ def turn(board)
   user_input = gets.strip
   index = input_to_index(user_input)
   if valid_move?(board, index) == true
-    move(board, index, value = "X")
+    move(board, index)
   else
     turn(board)
   end
